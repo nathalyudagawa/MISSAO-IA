97c6427826a15856bee869d6dcdc2337f559a851
nathalyudagawa <nathaly.udagawa@escola.pr.gov.br>
2026-09-21 14:12:44 +0000

    .

--- script.js
+++ script.js
@@ -5,27 +5,20 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  {
-    enunciado:
-      "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: [
-      {
-        texto: "Isso é assustador!",
-        afirmacao: [
-          "No início ficou com medo do que essa tecnologia pode fazer. ",
-          "Achou assustador pensar na velocidade na qual a tecnologia está avançando.",
-        ],
-      },
-      {
-        texto: "Isso é maravilhoso!",
-        afirmacao: [
-          "Quis saber como usar IA no seu dia a dia.",
-          "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
-        ],
-      },
-    ],
-  },
-];
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "afirmacao"
+            }           
+            
+        ]
+    },
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre elaIA. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude você toma?",
         alternativas: [
@@ -113,13 +106,6 @@ function respostaSelecionada(opcaoSelecionada){
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
-}
-
-function respostaSelecionada(opcaoSelecionada){
-        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
-        historiaFinal += afirmacoes + " ";
-        atual++;
-        mostraPergunta();
 }
 
 function mostraResultado(){
